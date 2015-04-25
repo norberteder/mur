7c9aca7617918d32a283fb62ba973480b36e867b
Norbert Eder <opensource@norberteder.com>
2015-04-25 12:52:56 +0200

    Add test console app

---
+++ src/Mur.TestApp/FibonacciJob.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mur.TestApp
+{
+    public class FibonacciJob : JobBase
+    {
+        public override void Run()
+        {
+            var fibOf10 = GetFibonacci(10);
+        }
+
+        public int GetFibonacci(int n)
+        {
+            if ((n == 0) || (n == 1))
+            {
+                return n;
+            }
+            else
+                return GetFibonacci(n - 1) + GetFibonacci(n - 2);
+        }
+    }
+}
---
+++ src/Mur.TestApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mur.TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exptectedJobId = "FibonacciJob";
+
+            var schedule = new Schedule()
+            {
+                Start = DateTime.Now,
+                Interval = "1 Minute"
+            };
+
+            var jobSettingsList = new List<JobSettings> {
+                new JobSettings {
+                    Id = exptectedJobId,
+                    Type = typeof(FibonacciJob).AssemblyQualifiedName,
+                    Schedules = new [] {
+                        schedule
+                    }
+                }
+            };
+
+            var scheduler = new Scheduler();
+            scheduler.JobFinished += (sender, arg) =>
+            {
+                if (arg.JobId == exptectedJobId)
+                {
+                    Console.WriteLine("Job '{0}' executed", arg.JobId);
+                }
+            };
+
+            scheduler.Start(jobSettingsList);
+
+            Console.ReadLine();
+
+        }
+    }
+}
